e1394704adef3f900e2f391eaa6e5be5f42095fc
Mark Spencer <markster@digium.com>
2004-01-17 15:14:11 +0000

    Fix SERVICE_ACKNOWLEDGE even with different address sizes
    git-svn-id: https://origsvn.digium.com/svn/libpri/trunk@71 2fbb986a-6c06-0410-b554-c9c1f0a7f128

--- q931.c
+++ q931.c
@@ -1935,7 +1935,7 @@ int q931_receive(struct pri *pri, q931_h *h, int len)
 		/* This is the weird maintenance stuff.  We majorly
 		   KLUDGE this by changing byte 4 from a 0xf (SERVICE) 
 		   to a 0x7 (SERVICE ACKNOWLEDGE) */
-		h->raw[3] -= 0x8;
+		h->raw[h->crlen + 2] -= 0x8;
 		q931_xmit(pri, h, len, 1);
 		return 0;
 	} else if (h->pd != Q931_PROTOCOL_DISCRIMINATOR) {
