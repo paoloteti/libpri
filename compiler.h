64eaca5bfabbb87e4481ae3d8bf0e6303e432be2
Kevin P. Fleming <kpfleming@digium.com>
2008-08-05 22:18:12 +0000

    Merged revisions 610 via svnmerge from https://origsvn.digium.com/svn/libpri/branches/1.2
    ........
    r610 | kpfleming | 2008-08-05 17:16:58 -0500 (Tue, 05 Aug 2008) | 2 lines
    
    clean up license headers, and explicitly grant additional permissions when used with Asterisk
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/libpri/branches/1.4@611 2fbb986a-6c06-0410-b554-c9c1f0a7f128

--- compiler.h
+++ compiler.h
@@ -4,9 +4,26 @@
  * Compiler-specific macros and other items
  *
  * Copyright (C) 2005, Digium, Inc.
+ */
+
+/*
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
  *
  * This program is free software, distributed under the terms of
- * the GNU General Public License
+ * the GNU General Public License Version 2 as published by the
+ * Free Software Foundation. See the LICENSE file included with
+ * this program for more details.
+ *
+ * In addition, when this program is distributed with Asterisk in
+ * any form that would qualify as a 'combined work' or as a
+ * 'derivative work' (but not mere aggregation), you can redistribute
+ * and/or modify the combination under the terms of the license
+ * provided with that copy of Asterisk, instead of the license
+ * terms granted here.
  */
 
 #ifndef _ASTERISK_COMPILER_H
--- pri_facility.h
+++ pri_facility.h
@@ -7,6 +7,26 @@
    Copyright (C) Digium, Inc. 2004-2005
 */
 
+/*
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2 as published by the
+ * Free Software Foundation. See the LICENSE file included with
+ * this program for more details.
+ *
+ * In addition, when this program is distributed with Asterisk in
+ * any form that would qualify as a 'combined work' or as a
+ * 'derivative work' (but not mere aggregation), you can redistribute
+ * and/or modify the combination under the terms of the license
+ * provided with that copy of Asterisk, instead of the license
+ * terms granted here.
+ */
+
 #ifndef _PRI_FACILITY_H
 #define _PRI_FACILITY_H
 #include "pri_q931.h"
