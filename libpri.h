4f0b3f3109d7e9767af08581372b7b77457732f7
Richard Mudgett <rmudgett@digium.com>
2009-06-26 19:41:56 +0000

    Doxygenify the timer comments.
    git-svn-id: https://origsvn.digium.com/svn/libpri/branches/1.4@919 2fbb986a-6c06-0410-b554-c9c1f0a7f128

--- libpri.h
+++ libpri.h
@@ -726,32 +726,32 @@ int pri_set_timer(struct pri *pri, int timer, int value);
 int pri_get_timer(struct pri *pri, int timer);
 int pri_timer2idx(const char *timer_name);
 
-/* New configurable timers and counters must be added to the end of the list */
+/*! New configurable timers and counters must be added to the end of the list */
 enum PRI_TIMERS_AND_COUNTERS {
-	PRI_TIMER_N200,	/* Maximum numer of Q.921 retransmissions */
-	PRI_TIMER_N201,	/* Maximum numer of octets in an information field */
-	PRI_TIMER_N202,	/* Maximum numer of transmissions of the TEI identity request message */
-	PRI_TIMER_K,	/* Maximum number of outstanding I-frames */
+	PRI_TIMER_N200,	/*!< Maximum numer of Q.921 retransmissions */
+	PRI_TIMER_N201,	/*!< Maximum numer of octets in an information field */
+	PRI_TIMER_N202,	/*!< Maximum numer of transmissions of the TEI identity request message */
+	PRI_TIMER_K,	/*!< Maximum number of outstanding I-frames */
 
-	PRI_TIMER_T200,	/* Time between SABME's */
-	PRI_TIMER_T201,	/* Minimum time between retransmissions of the TEI Identity check messages */
-	PRI_TIMER_T202,	/* Minimum time between transmission of TEI Identity request messages */
-	PRI_TIMER_T203,	/* Maximum time without exchanging packets */
+	PRI_TIMER_T200,	/*!< Time between SABME's */
+	PRI_TIMER_T201,	/*!< Minimum time between retransmissions of the TEI Identity check messages */
+	PRI_TIMER_T202,	/*!< Minimum time between transmission of TEI Identity request messages */
+	PRI_TIMER_T203,	/*!< Maximum time without exchanging packets */
 
 	PRI_TIMER_T300,
-	PRI_TIMER_T301,	/* Maximum time to respond to an ALERT */
+	PRI_TIMER_T301,	/*!< Maximum time to respond to an ALERT */
 	PRI_TIMER_T302,
-	PRI_TIMER_T303,	/* Maximum time to wait after sending a SETUP without a response */
+	PRI_TIMER_T303,	/*!< Maximum time to wait after sending a SETUP without a response */
 	PRI_TIMER_T304,
-	PRI_TIMER_T305,	/* Wait for DISCONNECT acknowledge */
+	PRI_TIMER_T305,	/*!< Wait for DISCONNECT acknowledge */
 	PRI_TIMER_T306,
 	PRI_TIMER_T307,
-	PRI_TIMER_T308,	/* Wait for RELEASE acknowledge */
-	PRI_TIMER_T309,	/* Time active calls can tollerate data link layer being down before clearing. */
-	PRI_TIMER_T310,	/* Maximum time between receiving a CALL_PROCEEDING and receiving a ALERT/CONNECT/DISCONNECT/PROGRESS */
-	PRI_TIMER_T313,	/* Wait for CONNECT acknowledge, CPE side only */
+	PRI_TIMER_T308,	/*!< Wait for RELEASE acknowledge */
+	PRI_TIMER_T309,	/*!< Time active calls can tollerate data link layer being down before clearing. */
+	PRI_TIMER_T310,	/*!< Maximum time between receiving a CALL_PROCEEDING and receiving a ALERT/CONNECT/DISCONNECT/PROGRESS */
+	PRI_TIMER_T313,	/*!< Wait for CONNECT acknowledge, CPE side only */
 	PRI_TIMER_T314,
-	PRI_TIMER_T316,	/* Maximum time between transmitting a RESTART and receiving a RESTART ACK */
+	PRI_TIMER_T316,	/*!< Maximum time between transmitting a RESTART and receiving a RESTART ACK */
 	PRI_TIMER_T317,
 	PRI_TIMER_T318,
 	PRI_TIMER_T319,
@@ -759,8 +759,8 @@ enum PRI_TIMERS_AND_COUNTERS {
 	PRI_TIMER_T321,
 	PRI_TIMER_T322,
 
-	PRI_TIMER_TM20,	/* Maximum time awaiting XID response */
-	PRI_TIMER_NM20,	/* Number of XID retransmits */
+	PRI_TIMER_TM20,	/*!< Maximum time awaiting XID response */
+	PRI_TIMER_NM20,	/*!< Number of XID retransmits */
 
 	/* Must be last in the enum list */
 	_PRI_MAX_TIMERS,
